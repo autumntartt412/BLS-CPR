691e2230fd520e19c252e70936a58ef8732095f2
Autumn <autumntartt.412@gmail.com>
2024-10-19 20:10:08 -0400

    html

---
+++ index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./dom.html";
+
+const newDiv = documernt.createElement('div'); 
+    newDiv.id = 'header';
+    newDiv.textContent = 'Don\'t Be Trife, Help Save a Life!';
+    newDiv.style.color = 'red';
+    document.body.appendChild('newDiv');
+    // document.getElementById('container').appendChild(newDiv);
+
+    
