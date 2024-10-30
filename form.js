5d1e0401a1bc6e118c1dd016c915ffed67ab97c8
Autumn <autumntartt.412@gmail.com>
2024-10-30 12:47:20 -0400

    errors

--- form.js
+++ form.js
@@ -19,49 +19,49 @@ const lastName = formEl.querySelector('input[name="lastname"]').value;
 const email = formEl.querySelector('input[name="email"]').value;
 
 
-const cardContainerEl = document.selectElementById('cardcontainer');
-cardContainerEl.style.height = '50%';
-cardContainerEl.style.width = '50%';
+// const cardContainerEl = document.selectElementById('cardcontainer');
+// cardContainerEl.style.height = '50%';
+// cardContainerEl.style.width = '50%';
 
 
-const navEl = document.selectElementById('nav');
-navEl.style.border = '5px white';
-navEl.classList.add('flex-around');
-navEl.style.textContent = 'white';
+// const navEl = document.selectElementById('nav');
+// navEl.style.border = '5px white';
+// navEl.classList.add('flex-around');
+// navEl.style.textContent = 'white';
 
-const priceCards = document.querySelectorAll('.cards');
-priceCards.style.width = '100px';
-priceCards.style.border = 'var(--text-color)';
-
-
-const buttonEl = document.selectElementById('button');
-buttonEl.style.padding = '30px';
+// const priceCards = document.querySelectorAll('.cards');
+// priceCards.style.width = '100px';
+// priceCards.style.border = 'var(--text-color)';
 
 
-for (let i = 0; i < cards.length; i++) {
-    const li = cards[i].querySelector("li");
-    if (li && li.textContent === "Infant CPR/AED - $50") {
-        const newList = cards[i].querySelectorAll("li");
-        newList.forEach((dogCpr) => {
-            dogCpr.textContent = "Doggy CPR - $50";
-        });
-    }
-}
-
-const listEls = document.querySelectorAll('li');
-listEls.forEach(el => {
-    el.style.fontWeight = 'bold';
-    el.style.alignContent = 'left';
-});
+// const buttonEl = document.selectElementById('button');
+// buttonEl.style.padding = '30px';
 
 
-const ul = document.li;
-appendChild(document.createElement('div'));
-appendChild(document.createElement('ul'));
+// for (let i = 0; i < cards.length; i++) {
+//     const li = cards[i].querySelector("li");
+//     if (li && li.textContent === "Infant CPR/AED - $50") {
+//         const newList = cards[i].querySelectorAll("li");
+//         newList.forEach((dogCpr) => {
+//             dogCpr.textContent = "Doggy CPR - $50";
+//         });
+//     }
+// }
 
-for (let i = 0; i < 2; i++) {
-    ul.appendChild.createElement('li')
-};
+// const listEls = document.querySelectorAll('li');
+// listEls.forEach(el => {
+//     el.style.fontWeight = 'bold';
+//     el.style.alignContent = 'left';
+// });
+
+
+// const ul = document.li;
+// appendChild(document.createElement('div'));
+// appendChild(document.createElement('ul'));
+
+// for (let i = 0; i < 2; i++) {
+//     ul.appendChild.createElement('li')
+// };
 
 
  
@@ -283,21 +283,21 @@ function validateCprFormOnSubmit(e) {
 validateCprFormOnSubmit();
 
 
-const formData = new FormData(document.getElementById('submit'));
-const data = {};
+// const formData = new FormData(document.getElementById('submit'));
+// const data = {};
 
-formData.forEach((value, key) => {
-    data[key] = value;
-});
-
-
-const priceList = document.querySelectorAll('pricelist');
-let newPriceListItem = document.createElement('li');
-newPriceListItem.textContent = "First Aid - $50";
-priceList.appendChild(newPriceListItem);
+// formData.forEach((value, key) => {
+//     data[key] = value;
+// });
 
 
-addPriceListItem("First Aid", 50);
+// const priceList = document.querySelectorAll('pricelist');
+// let newPriceListItem = document.createElement('li');
+// newPriceListItem.textContent = "First Aid - $50";
+// priceList.appendChild(newPriceListItem);
+
+
+// addPriceListItem("First Aid", 50);
 
 
   const newH4 = document.createElement('h4');
@@ -321,27 +321,5 @@ addPriceListItem("First Aid", 50);
 
   
 
-// let window;
 
-// function newWindow() {
-//   window = window.open(
-//     "file:///C:/Users/tseh/Documents/Perscholas/SBADomManipulation/form.html?firstname=&lastname=&email=",
-//     "form",
-//     `width=${availW * 0.75}, height=${availH * 0.75}, left=${
-//       availW * 0.125
-//     }, top=${availH * 0.125}, resizable=yes, scrollbars=yes, location=yes`
-//   );
-//   window.focus();
-// }
-// newWindow(window);
-
-// function closeWindow() {
-//   window.close();
-// }
-// closeWindow();
-
-// document.getElementById("openWindowBtn").addEventListener("click", newWindow);
-// document
-//   .getElementById("closeWindowBtn")
-//   .addEventListener("click", closeWindow);
 
